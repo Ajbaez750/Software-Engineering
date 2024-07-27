5d482b0de928f0ad7d7305da877ae5423e46f241
ajbae <ajbaez750@gmail.com>
2024-07-27 17:03:46 -0400

    Final product

--- main.py
+++ main.py
@@ -13,3 +13,23 @@ def decode(password):
         changed_char = (int(char) - 3) % 10
         decode_password += str(changed_char)
     return decode_password
+def main():
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        menu = int(input("Please enter an option: "))
+        if menu == 1:
+            password = input("Please enter your password to encode: ")
+            val = encode(password)
+            print("Your password has been encoded and stored!")
+        elif menu == 2:
+            if val:
+                decode_password = decode(val)
+                print(f"The encoded password is {val}, and the original password is {decode_password}.")
+        elif menu == 3:
+            break
+if __name__ == "__main__":
+    main()
