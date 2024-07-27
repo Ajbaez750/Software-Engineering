4ba83aa37aa5dd975aaa264172f3753420148213
christopheranderson <christopher33154@gmail.com>
2024-07-27 16:34:28 -0400

    I added the decode function to my partner's code.

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def encode(password):
         new_char = (int(char)+3)%10
         empty_string += str(new_char)
     return empty_string
+
+def decode(password):
+    decode_password = ""
+    password = str(password)
+    for char in password:
+        changed_char = (int(char) - 3) % 10
+        decode_password += str(changed_char)
+    return decode_password
