f5b5c658c641ba01a5d66afe3a0f890f5a8838b7
ajbae <ajbaez750@gmail.com>
2024-07-27 15:59:46 -0400

    added main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("yo")
+def encode(password):
+    empty_string = ""
+    password = str(password)
+    for char in password:
+        new_char = (int(char)+3)%10
+        empty_string += str(new_char)
+    return empty_string
+print(encode(125987))
